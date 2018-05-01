efa0a14f358c22eea43a6f2b43d5ae2486fbb1a9
NERVENERD\NerveNerd <dnsudhir@gmail.com>
2018-05-02 01:18:53 +0530

    Working Project

--- app/src/main/java/anupam/acrylic/Splash.java
+++ app/src/main/java/anupam/acrylic/Splash.java
@@ -1,51 +1,31 @@
-/*
- * Copyright (C) 2014 Valerio Bozzolan
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package anupam.acrylic;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import anupam.acrylic.R;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		setContentView(R.layout.main);
+  private Handler handler;
+  private Runnable runnable;
 
-		Thread t = new Thread() {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
 
-			public void run() {
-				try {
-					Thread.sleep(1000);
+    handler = new Handler();
+    runnable = new Runnable() {
+      @Override public void run() {
+        startActivity(new Intent(Splash.this, EasyPaint.class));
+        finish();
+      }
+    };
+    handler.postDelayed(runnable, 1000);
+  }
 
-					startActivity(new Intent().setClassName("anupam.acrylic",
-							"anupam.acrylic.EasyPaint"));
-					finish();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		t.start();
-	}
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    handler.removeCallbacks(runnable);
+  }
 }
